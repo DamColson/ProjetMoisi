09bf6c1a68ad6abacc1639036e629d5e8724858d
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-05 16:05:39 +0200

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$fileXml = 'source.xml';
+$xml = simplexml_load_file($fileXML);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -10,6 +15,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
         <title>warFriendsForm</title>
     </head>
+
+<?php
+$xml->page[1]->menu
+?>
+
     <body>
     <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
