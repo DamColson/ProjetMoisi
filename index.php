6052426b9ab48cf85d312ab2736806f7348ccfa7
PatrickSohyer <p.sohyer@gmail.com>
2019-06-06 09:42:09 +0200

    Commit à 10h

--- index.php
+++ index.php
@@ -56,7 +56,6 @@ $xml = simplexml_load_file($file);
 
         </div>
 
-<?= var_dump($xml); ?>
 
 
         <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
